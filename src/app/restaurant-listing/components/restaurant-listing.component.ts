eba1f8058642efdb76164cb126206b51209ed2fd
RAMIREDDY HARI GOUTHAM REDDY <46675976+HariGoutham@users.noreply.github.com>
2025-02-14 21:05:52 +0800

    Update restaurant-listing.component.ts

--- src/app/restaurant-listing/components/restaurant-listing.component.ts
+++ src/app/restaurant-listing/components/restaurant-listing.component.ts
@@ -25,6 +25,7 @@ export class RestaurantListingComponent {
       }
     )
   }
+  
   getRandomNumber(min: number, max: number): number {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
