4b4a3f1b41123b7bb4d108e9b9f41737b02fe595
RAMIREDDY HARI GOUTHAM REDDY <46675976+HariGoutham@users.noreply.github.com>
2025-02-14 19:50:17 +0800

    Update header.module.ts

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 import { HeaderRoutingModule } from './header-routing.module';
 import { HeaderComponent } from './components/header.component'; 
 
-
 @NgModule({
   declarations: [
     HeaderComponent
